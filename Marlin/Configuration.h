c27e3aa4f0337329bf4d208a55a222412d018a9f
noah978 <noahkeck@mindspring.com>
2020-04-16 16:49:04 -0500

    Switch to 4 x 4 grid of bed leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -960,7 +960,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -11, -4.100 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -11, -4.150 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1255,7 +1255,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 4
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
